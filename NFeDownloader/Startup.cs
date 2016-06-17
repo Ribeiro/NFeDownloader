1aecb1ceca57ad60999009e85f2e900f8476d9ea
Janny <Janny@JANNY_DELL>
2016-06-17 00:31:41 -0300

    Add project files.

---
+++ NFeDownloader/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NFeDownloader.Startup))]
+namespace NFeDownloader
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
